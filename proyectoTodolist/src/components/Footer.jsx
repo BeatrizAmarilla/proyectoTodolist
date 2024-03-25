3c68734499d7b90eb0217962c8f52566fec7751c
beatriz amarilla <betty.182.186@gmail.com>
2024-03-24 21:59:44 -0300

    add footer

---
+++ proyectoTodolist/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { FaHeart } from "react-icons/fa";
+
+export default function header() {
+  return (
+    <Typography mt={10}>
+      Hecho con <FaHeart/>  por Beatriz
+    </Typography>
+  );
+}
