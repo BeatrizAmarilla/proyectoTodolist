2db482893ae98881fbbd9b48235c8c866ce88df2
beatriz amarilla <betty.182.186@gmail.com>
2024-03-26 22:02:28 -0300

    fix-style-app

--- proyectoTodolist/src/components/AddTask.jsx
+++ proyectoTodolist/src/components/AddTask.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import SendIcon from "@mui/icons-material/Send";
 import { FaCheck, FaTrash } from "react-icons/fa";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import NativeSelect from "@mui/material/NativeSelect";
-import { Stack, Typography } from "@mui/material";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  MenuItem,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 export default function AddTask() {
   const [tasks, setTasks] = useState(
@@ -15,8 +19,13 @@ export default function AddTask() {
   );
   const [nombre, setNombre] = useState("");
   const [filterOption, setFilterOption] = useState("all");
+  const [error, setError] = useState("");
 
   function AddNewTask() {
+    if (nombre.length < 5) {
+      return;
+    }
+
     const newTask = [
       ...tasks,
       { id: crypto.randomUUID(), nombre, completed: false },
@@ -52,106 +61,170 @@ export default function AddTask() {
     }
     return true;
   }
-  
-  return (
-    <>
-      <Stack direction={{md:"row",xs:"column"}} flexWrap={"wrap"} width={"100%"} gap={"1rem"}>
-      <TextField
-        sx={{backgroundColor: "white", mb: "10px",width:{md:"49%",xs:"100%"}}}
-        required
-        id="filled-required"
-        label="TAREA"
-        defaultValue="INGRESE UNA TAREA"
-        variant="filled"
-        onChange={(e) => setNombre(e.target.value)}
-      />
 
-      <FormControl sx={{backgroundColor: "white",width:{md:"49%",xs:"100%"}}}>
-        <InputLabel variant="standard" htmlFor="uncontrolled-native">
-          SELECCIONAR
-        </InputLabel>
-        <NativeSelect
-          defaultValue={10}
-          inputProps={{
-            name: "TASK",
-            id: "uncontrolled-native",
+  const selectOption = [
+    {
+      value: "all",
+      label: "Todas",
+    },
+    {
+      value: "completed",
+      label: "Completada",
+    },
+    {
+      value: "incomplete",
+      label: "Incompleta",
+    },
+  ];
+  console.log(error);
+  return (
+    <Stack
+      direction={{ md: "row", xs: "column" }}
+      flexWrap={"wrap"}
+      width={"100%"}
+      gap={2}
+    >
+      <Stack direction={{ xs: "column", md: "row" }} width={"100%"} spacing={2}>
+        <FormControl sx={{ width: { md: "49%", xs: "100%" } }}>
+          <FormLabel sx={{ color: "white" }}>Agregar Tarea</FormLabel>
+
+          <TextField
+            sx={{
+              backgroundColor: "white",
+            }}
+            required
+            id="filled-required"
+            placeholder="Correr"
+            variant="filled"
+            onChange={(e) => {
+              setNombre(e.target.value);
+              if (e.target.value.length < 5) {
+                setError("La tarea debe tener al menos 5 caracteres.");
+              } else {
+                setError("");
+              }
+            }}
+            error={Boolean(error)} 
+            helperText={error} 
+            inputProps={{ maxLength: 30 }}
+          />
+        </FormControl>
+
+        <FormControl sx={{ width: { md: "49%", xs: "100%" } }}>
+          <FormLabel sx={{ color: "white" }}>Seleccionar Tipo de Tarea</FormLabel>
+          <TextField
+            sx={{ backgroundColor: "white" }}
+            id="outlined-select-currency"
+            select
+            placeholder="Seleccione tipo de tarea"
+            onChange={(e) => setFilterOption(e.target.value)}
+            defaultValue="all"
+          >
+            {selectOption.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </FormControl>
+      </Stack>
+
+      <Stack
+        sx={{
+          justifyContent: { xs: "center", md: "flex-start" },
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Button
+          variant="contained"
+          endIcon={<SendIcon />}
+          onClick={AddNewTask}
+          sx={{
+            background: "#4449D8",
+            textTransform: "none",
+            "&:hover": {
+              backgroundColor: "#4449D8",
+            },
+            width: { md: "20%", xs: "100%" },
           }}
-          onChange={(e) => setFilterOption(e.target.value)}
         >
-          <option value="all">TODAS</option>
-          <option value="completed">COMPLETA</option>
-          <option value="incomplete">INCOMPLETA</option>
-        </NativeSelect>
-      </FormControl>
-      <Stack sx={{alignItems:"center",width:"100%"}}>
-      <Button variant="contained" endIcon={<SendIcon />} onClick={AddNewTask} sx={{alignSelf:"center",background:"#3f51b5"}} fullWidth>
-        Send
-      </Button>
+          Agregar
+        </Button>
       </Stack>
 
       {tasks.filter(filterTasks).map((task) => (
         <Stack
           key={task.id}
-          width={{md:"80%",xs:"100%"}}
+          width={{ md: "80%", xs: "100%" }}
           margin={"0 auto"}
           direction={"row"}
           sx={{
-            justifyContent:"space-between",
-            alignItems:"center",
-           
-            backgroundColor: "white",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "10px",
+            borderRadius: "8px",
+            backgroundColor: "aliceblue",
             color: "black",
-            gap:"4px",
-            boxSizing:"border-box",
+            gap: "4px",
+            boxSizing: "border-box",
           }}
         >
-        <Stack width={"69%"} flexWrap={"nowrap"}> <Typography sx={{textDecoration: task.completed ? "line-through" : "none",}}
-        textAlign={""}>{task.nombre}</Typography></Stack>
-          
-        <Stack direction={"row"} width={"29%"} justifyContent={"center"} alignItems={"end"} gap={"4px"}>
-          <Button
-            onClick={() => markTaskComplete(task.id)}
-            size="small"
-            sx={{
-              backgroundColor: task.completed ? "grey" : "green",
-              color: "white",
-              width:"49%",
-              padding:"8px",
-              minWidth:"",
-              fontSize:"12px",
-            }}
-          >
-             <FaCheck /> 
-          </Button>
+          <Stack width={"69%"} flexWrap={"nowrap"}>
+            <Typography
+              sx={{
+                textDecoration: task.completed ? "line-through" : "none",
+              }}
+            >
+              {task.nombre}
+            </Typography>
+          </Stack>
 
-          <Button
-            onClick={() => deleteTask(task.id)}
-            size="small"
-            sx={{
-              backgroundColor: "black",
-              color: "white",
-              width:"49%",
-              padding:"8px",
-              minWidth:"",
-              fontSize:"12px",
-            }}
+          <Stack
+            direction={"row"}
+            width={"29%"}
+            justifyContent={"center"}
+            alignItems={"end"}
+            gap={"4px"}
           >
-            <FaTrash />
-          </Button>
+            <Button
+              onClick={() => markTaskComplete(task.id)}
+              size="small"
+              sx={{
+                backgroundColor: task.completed ? "grey" : "green",
+                color: "white",
+                width: "49%",
+                padding: "8px",
+                minWidth: "",
+                fontSize: "12px",
+                "&:hover": {
+                  backgroundColor: task.completed ? "grey" : "green",
+                },
+              }}
+            >
+              <FaCheck />
+            </Button>
+
+            <Button
+              onClick={() => deleteTask(task.id)}
+              size="small"
+              sx={{
+                backgroundColor: "red",
+                color: "white",
+                width: "49%",
+                padding: "8px",
+                minWidth: "",
+                fontSize: "12px",
+                "&:hover": {
+                  backgroundColor: "red",
+                },
+              }}
+            >
+              <FaTrash fontSize={"12px"} />
+            </Button>
           </Stack>
         </Stack>
-      ))} 
-
-
-
-
-
-
-      </Stack>
-
-
-
-      
-    </>
+      ))}
+    </Stack>
   );
 }
--- proyectoTodolist/src/components/Footer.jsx
+++ proyectoTodolist/src/components/Footer.jsx
@@ -3,10 +3,25 @@ import { FaHeart } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <Stack mt={"2rem"} justifyContent={"center"} width={"100%"} padding={"10px 0"} mb={"2rem"}>
-    <Typography color={"white"} textAlign={"center"}>
-      Hecho con <FaHeart/>  por Beatriz
-    </Typography>
+    <Stack
+      mt={"2rem"}
+      justifyContent={"center"}
+
+
+      width={"100%"}
+      padding={"10px 0"}
+      mb={"2rem"}
+      position={"relative"}
+      direction={"row"}
+      gap={1}
+      color={"white"}
+    >
+      <Typography textAlign={"center"}>Hecho con </Typography>
+
+      <Typography>
+        <FaHeart />
+      </Typography>
+      <Typography> por Beatriz</Typography>
     </Stack>
   );
 }
